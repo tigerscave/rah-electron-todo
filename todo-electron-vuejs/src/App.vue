91416a578008c1595201428f76dbb1ec32f04bdc
rhmt <rezaeihazara@gmail.com>
2019-04-03 22:33:55 +0900

    config(vue.js): install vue js

---
+++ todo-electron-vuejs/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    {{msg}}
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'app',
+  components: {
+  },
+  data() {
+    return {
+      msg: 'Hello World'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
